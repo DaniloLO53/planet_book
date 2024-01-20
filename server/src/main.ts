747594789420f6db72b926133b56cb30f0807602
Danilo Leão <daniloleaodev@gmail.com>
2024-01-20 11:26:58 -0300

    feat>: main

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT);
+  const configService = app.get(ConfigService);
+  app.use(cookieParser(configService.get<string>('COOKIE_SECRET')));
+  app.use(helmet());
+  app.enableCors({
+    credentials: true,
+    origin: `https://${configService.get<string>('domain')}`,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  await app.listen(configService.get<number>('port'));
 }
+
 bootstrap();
