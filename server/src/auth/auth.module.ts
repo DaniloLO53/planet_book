aeec9af20476529a7d53433c3b31e580067e1247
Danilo Leão <daniloleaodev@gmail.com>
2024-01-20 10:37:17 -0300

    feat: auth module

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { JwtModule } from '../jwt/jwt.module';
 import { MailerModule } from '../mailer/mailer.module';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [UsersModule, JwtModule, MailerModule],
   providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
